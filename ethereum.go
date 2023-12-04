16c31c86b6c9c98ae1489c15ee7dfaa4ac189d10
Sasha Duke <sashadanielduke@gmail.com>
2023-12-04 22:45:06 +0000

    minor updates

--- ethereum.go
+++ ethereum.go
@@ -13,25 +13,28 @@ import (
 )
 
 const (
-	StorageContractAddress = "0x48eB2302cfEc7049820b66FC91955C5d250b3fF9"
+	StorageContractAddress = "48eB2302cfEc7049820b66FC91955C5d250b3fF9"
 	SepoliaRPCEndpoint     = "https://sepolia.infura.io/v3/131bd995e0764b2da6be91ee9058dc91"
 	ECDSAPrivkeyHex        = "fe05041e74295604ff8f76dc24847c06e93c015da608b4281446c7de6f54cc46"
 )
 
-// Keeper logic to interact with Ethereum blockchain.
+// Keeper logic to interact with Ethereum blockchain (Sepolia Testnet)
 func keeper() {
 	client, auth, err := connectToEthereum()
 	if err != nil {
 		log.Fatalf("ethereum connection error: %v\n", err)
 	}
 
-	contractAddress := common.HexToAddress(StorageContractAddress[2:])
+	contractAddress := common.HexToAddress(StorageContractAddress)
 	contract, err := instantiateContract(client, contractAddress)
 	if err != nil {
 		log.Fatalf("contract instantiation error: %v\n", err)
 	}
 
 	for {
+		// Prevents us spending too much gas or getting rate-limited by RPC provider
+		time.Sleep(20 * time.Second)
+
 		nonce, err := client.PendingNonceAt(context.Background(), auth.From)
 		if err != nil {
 			log.Fatalf("nonce error: %v\n", err)
@@ -43,7 +46,7 @@ func keeper() {
 
 		auth.Nonce = big.NewInt(int64(nonce))
 		auth.Value = big.NewInt(0)      // in wei
-		auth.GasLimit = uint64(3000000) // in units
+		auth.GasLimit = uint64(3000000) // in gas units
 		auth.GasPrice = gasPrice
 
 		data, err := retrieveDataRedis()
@@ -55,14 +58,11 @@ func keeper() {
 		if err != nil {
 			log.Fatalf("contract call error: %v\n", err)
 		}
-		log.Printf("Transaction sent! Tx hash: %s\n\n", tx.Hash().Hex())
-
-		// Prevents rate-limiting from the RPC provider and spending too much gas
-		time.Sleep(10 * time.Second)
+		log.Printf("\nPrice update sent to blockchain! Tx hash:\n%s\n\n", tx.Hash().Hex())
 	}
 }
 
-// connectToEthereum establishes a connection to an Ethereum client and creates an authenticated session.
+// connectToEthereum establishes a connection to an Ethereum client and creates an authenticated session
 func connectToEthereum() (*ethclient.Client, *bind.TransactOpts, error) {
 	client, err := ethclient.Dial(SepoliaRPCEndpoint)
 	if err != nil {
@@ -87,6 +87,7 @@ func connectToEthereum() (*ethclient.Client, *bind.TransactOpts, error) {
 	return client, auth, nil
 }
 
+// instantiateContract creates an object with contract methods using generated ABI bindings
 func instantiateContract(client *ethclient.Client, address common.Address) (*Storage, error) {
 	contract, err := NewStorage(address, client)
 	if err != nil {
